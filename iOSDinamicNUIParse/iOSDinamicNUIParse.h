3c5bf63c8e599609ff3fee5fd2fe4d4bd57ac635
Caio Dias <kiobreno@gmail.com>
2016-07-15 12:56:51 -0400

    Trying

---
+++ iOSDinamicNUIParse/iOSDinamicNUIParse.h
@@ -0,0 +1,19 @@
+//
+//  iOSDinamicNUIParse.h
+//  iOSDinamicNUIParse
+//
+//  Created by Caio Dias on 2016-07-14.
+//  Copyright © 2016 In The Beginning... All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSDinamicNUIParse.
+FOUNDATION_EXPORT double iOSDinamicNUIParseVersionNumber;
+
+//! Project version string for iOSDinamicNUIParse.
+FOUNDATION_EXPORT const unsigned char iOSDinamicNUIParseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSDinamicNUIParse/PublicHeader.h>
+
+
---
+++ iOSNUIParse/iOSNUIParse/iOSNUIParse.h
@@ -0,0 +1,19 @@
+//
+//  iOSNUIParse.h
+//  iOSNUIParse
+//
+//  Created by Caio Dias on 2016-07-14.
+//  Copyright © 2016 Life Tales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSNUIParse.
+FOUNDATION_EXPORT double iOSNUIParseVersionNumber;
+
+//! Project version string for iOSNUIParse.
+FOUNDATION_EXPORT const unsigned char iOSNUIParseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSNUIParse/PublicHeader.h>
+
+
